fa11df432422e9ea824011d30a3c4430c4862074
Charles Dusek <dusek@onomy.io>
2024-04-17 19:11:20 -0500

    Add in first section of create-pool msg logic

--- x/market/keeper/msg_server_create_pool.go
+++ x/market/keeper/msg_server_create_pool.go
@@ -4,6 +4,7 @@ import (
 	"context"
 
 	"onex/x/market/types"
+	"strings"
 
 	sdk "github.com/cosmos/cosmos-sdk/types"
 )
@@ -11,7 +12,26 @@ import (
 func (k msgServer) CreatePool(goCtx context.Context, msg *types.MsgCreatePool) (*types.MsgCreatePoolResponse, error) {
 	ctx := sdk.UnwrapSDKContext(goCtx)
 
-	// TODO: Handling the message
+	// CoinAmsg and CoinBmsg pre-sort from raw msg
+	coinA, err := sdk.ParseCoinNormalized(msg.CoinA)
+	if err != nil {
+		panic(err)
+	}
+
+	coinB, err := sdk.ParseCoinNormalized(msg.CoinB)
+	if err != nil {
+		panic(err)
+	}
+
+	coinPair := sdk.NewCoins(coinA, coinB)
+
+	// NewCoins sorts denoms.
+	// The sorted pair joined by "," is used as the key for the pool.
+	denom1 := coinPair.GetDenomByIndex(0)
+	denom2 := coinPair.GetDenomByIndex(1)
+	pair := strings.Join([]string{denom1, denom2}, ",")
+
+	_ = pair
 	_ = ctx
 
 	return &types.MsgCreatePoolResponse{}, nil
